3dfc7b3b43085152bdb3b5ad25afb1635c112390
Tony Distinti <25881824+breadoliveoilsalt@users.noreply.github.com>
2020-04-16 12:18:48 -0400

    Add message to confirm travis deploy on merge into master

--- App.js
+++ App.js
@@ -5,6 +5,7 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text testID="text">Open up App.js to start working on your app!</Text>
+      <Text>This confirms Travis only deploys when merged into master.</Text>
     </View>
   );
 }
