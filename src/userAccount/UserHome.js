151e4594d411777362ea45b5206933e43b27f076
Tony Distinti <25881824+breadoliveoilsalt@users.noreply.github.com>
2020-04-22 12:53:34 -0400

    Add text and formatting to UserHome

--- src/userAccount/UserHome.js
+++ src/userAccount/UserHome.js
@@ -1,10 +1,15 @@
-import React, { Component }from 'react'
+import React from 'react'
 import { StyleSheet, Text, View, TouchableOpacity } from 'react-native'
+import Logo from '../components/Logo'
 
 const UserHome = (props) => {
   return (
-    <View style={styles.container}>
-      <Text>You signed in!</Text>
+    <View style={styles.container} >
+      <Logo/>
+      <Text style={styles.text} >Nice to see you!</Text>
+      <Text style={styles.text} >Please click Browse to rate items.</Text>
+      <Text style={styles.text} >Then click Compare to see what items you and another user have both liked!</Text>
+      <Text style={styles.text} >If you ever want to sign out, click below.</Text>
       <TouchableOpacity
         onPress={props.renderSignedOutView}
       >
@@ -21,5 +26,14 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
+    backgroundColor: "#3484F2"
+  },
+  text: {
+    color: "#FFDD1F",
+    fontSize: 25,
+    textAlign: "center", 
+    marginLeft: "5%",
+    marginRight: "5%",
+    marginTop: "3%"
   }
 })
