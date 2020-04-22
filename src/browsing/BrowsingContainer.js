a962a5fec077cb158bb4bb632a10e41424acf071
Tony Distinti <25881824+breadoliveoilsalt@users.noreply.github.com>
2020-04-22 17:02:35 -0400

    Render raw JSON of items to browse in BrowsingContainer

--- src/browsing/BrowsingContainer.js
+++ src/browsing/BrowsingContainer.js
@@ -1,13 +1,34 @@
 import React, { Component } from 'react'
 import { StyleSheet, Text, View } from 'react-native'
+import { getItemsToBrowse } from '../api/apiRequests'
+import { getToken } from '../userAccount/tokenActions'
 
 class BrowsingContainer extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      token: null,
+      itemsToBrowse: null,
+      messages: [],
+      messagesModalVisible: false
+    }
+  }
+
+  componentDidMount() {
+    getToken()
+      .then(token => this.setState({token: token}))
+      .then(() => getItemsToBrowse({token: this.state.token}))
+      .then(items => this.setState({itemsToBrowse: items}))
+      .catch(() => console.log("There was a problem getting the items to browse"))
+  }
 
   render() {
     //getItemsToBrowse only requires {token: token} as params
     return (
       <View style={styles.container} >
         <Text>Browsing Container</Text>
+        {this.state.itemsToBrowse ? <Text>{JSON.stringify(this.state.itemsToBrowse)}</Text> : null}
       </View>
     )
   }
