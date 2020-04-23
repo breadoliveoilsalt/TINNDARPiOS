d0d3ec9ac2fc8829b9f3773696fa9595ca1d2667
Tony Distinti <25881824+breadoliveoilsalt@users.noreply.github.com>
2020-04-22 20:45:36 -0400

    Spike swipeable image to change background, with dx, vx, and screenWidth displayed

--- src/browsing/BrowsingContainer.js
+++ src/browsing/BrowsingContainer.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, PanResponder, Animated, Dimensions } from 'react-native'
 import { getItemsToBrowse } from '../api/apiRequests'
 import { getToken } from '../userAccount/tokenActions'
 
@@ -7,12 +7,52 @@ class BrowsingContainer extends Component {
 
   constructor(props) {
     super(props)
+    this.translateX = new Animated.Value(0)
+    this.panResponder = PanResponder.create({
+      onMoveShouldSetResponderCapture: () => true,
+      onMoveShouldSetPanResponderCapture: () => true,
+      onPanResponderMove: Animated.event([null, { dx: this.translateX }]),
+      onPanResponderRelease: (e, { vx, dx }) => {
+        const screenWidth = Dimensions.get("window").width
+        this.setState({screenWidth: screenWidth})
+        this.setState({ dx: dx }) // dx - accumulated distance of the gesture since the touch started (x axis)
+        this.setState({ vx: vx }) // vx - current velocity of the gesture (x axis)
+        if (this.sufficientRightSwipe(screenWidth, vx, dx)) {
+          Animated.timing(this.translateX, {
+            toValue: dx > 0 ? screenWidth : -screenWidth,
+            duration: 200
+          }).start(this.setState({backgroundColor: {backgroundColor: "yellow"}}))
+        } else if (this.sufficientLeftSwipe(screenWidth, vx, dx)) {
+          Animated.timing(this.translateX, {
+            toValue: dx > 0 ? screenWidth : -screenWidth,
+            duration: 200
+          }).start(this.setState({backgroundColor: {backgroundColor: "blue"}}))
+        } else {
+          Animated.spring(this.translateX, {
+            toValue: 0,
+            bounciness: 10
+          }).start()
+        }
+      }
+    })
     this.state = {
       token: null,
       itemsToBrowse: null,
       messages: [],
-      messagesModalVisible: false
+      messagesModalVisible: false,
+      dx: 0,
+      vx: 0,
+      screenWidth: 0,
+      backgroundColor: {backgroundColor: "red"}
     }
+  }
+
+  sufficientRightSwipe(screenWidth, xAxisVelocity, xAxisDistance) {
+    return xAxisVelocity >= 0.5 || xAxisDistance >= 0.5 * screenWidth
+  }
+
+  sufficientLeftSwipe(screenWidth, xAxisVelocity, xAxisDistance) {
+    return xAxisVelocity <= -0.5 || xAxisDistance <= -0.5 * screenWidth
   }
 
   componentDidMount() {
@@ -25,12 +65,28 @@ class BrowsingContainer extends Component {
 
   render() {
     //getItemsToBrowse only requires {token: token} as params
-    return (
-      <View style={styles.container} >
-        <Text>Browsing Container</Text>
-        {this.state.itemsToBrowse ? <Text>{JSON.stringify(this.state.itemsToBrowse)}</Text> : null}
-      </View>
-    )
+    if (this.state.itemsToBrowse) {
+      return (
+        <View style={{...styles.container, ...this.state.backgroundColor}} >
+          <Text>Browsing Container</Text>
+          <Text>DX: {this.state.dx}</Text>
+          <Text>VX: {this.state.vx}</Text>
+          <Text>screenWidth: {this.state.screenWidth}</Text>
+
+          <Animated.Image 
+           source={{uri: this.state.itemsToBrowse[0].imageURL}}
+           style={{ transform: [{ translateX: this.translateX }], ...styles.image}} 
+           {...this.panResponder.panHandlers}
+           />
+        </View>
+      )
+    } else {
+        <View style={styles.container} >
+          <Text>No images!</Text>
+        </View>
+      return null
+    }
+
   }
 }
 
@@ -41,5 +97,10 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
+  },
+  image: {
+    height: 250,
+    width: 250,
+    margin: "5%",
   }
 })
